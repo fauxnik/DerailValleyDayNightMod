db4290c7837868e9cacdd141e3afbb6e5b902358
Niko Fox <niko@fauxnik.me>
2020-07-15 14:31:58 -0700

    wip: sun directional light moves, skybox rotates & dims, terrian & objects still illuminated tho

--- DayNightMod/Main.cs
+++ DayNightMod/Main.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityModManagerNet;
+using VRTK;
 
 namespace DayNightMod
 {
@@ -11,6 +14,7 @@ namespace DayNightMod
         // altitude = x, azimuth = y
         static float culminationAltitude = 72f;
         static float nadirAltitude = -72f;
+        // TODO: make use of these
         static float sunriseAzimuth = -87f;
         static float sunsetAzimuth = 87f;
         static float dayIntensity = 2.4f;
@@ -21,6 +25,13 @@ namespace DayNightMod
         static float defaultAzimuth = 68.8f;
         static float defaultIntensity = 2.4f;
 
+        // skybox defaults
+        static float defaultRotation = 113f;
+        static float defaultExposure = 1f;
+
+        // color grading defaults
+        static float defaultBrightness = 0f;
+
         // TODO: https://sunrise-sunset.org/api
         static float sunriseTime = 21600f;
         static float sunsetTime = 64800f;
@@ -29,10 +40,46 @@ namespace DayNightMod
         const float secondsInDay = 86400f;
 
         static Light sun = null;
+        static ColorGrading colorGrading = null;
+        //static AssetBundle assetBundle;
+        static Texture[] skybox;
 
         static bool Load(UnityModManager.ModEntry modEntry)
 		{
+            // TODO: figure out how to use an asset bundle
+            //string assetBundlePath = modEntry.Path + "Resources\\skybox";
+            //assetBundle = AssetBundle.LoadFromFile(assetBundlePath);
+            //if (assetBundle == null)
+            //    modEntry.Logger.Error("failed to load skybox asset bundle [" + assetBundlePath + "]");
+            //else
+            //    modEntry.Logger.Log("loaded skybox asset bundle");
+
+            skybox = new Texture[6];
+            var sides = new string[] { "Front", "Back", "Left", "Right", "Top", "Bottom" };
+            for (int i = 0; i < sides.Length; i++)
+			{
+                string side = sides[i];
+                string filePath = modEntry.Path + "Resources\\Dusk_" + side + ".png";
+                byte[] fileData = File.ReadAllBytes(filePath);
+                
+                if (fileData == null)
+				{
+                    modEntry.Logger.Error(string.Format(
+                        "failed to load skybox side ({0})",
+                        filePath));
+                    return false;
+                }
+
+                var texture = new Texture2D(1024, 1024);
+
+                texture.LoadImage(fileData);
+                texture.Apply(true, true);
+
+                skybox[i] = texture;
+            }
+
             modEntry.OnToggle = OnToggle;
+
             return true;
         }
 
@@ -40,30 +87,31 @@ namespace DayNightMod
 		{
             if (isTogglingOn)
             {
-                modEntry.OnFixedUpdate = OnFixedUpdate;
+                modEntry.OnUpdate = OnUpdate;
                 return true;
 			}
 
-            modEntry.OnFixedUpdate = null;
+            modEntry.OnUpdate = null;
             UpdateSun(defaultAltitude, defaultAzimuth, defaultIntensity);
+            UpdateSkybox(defaultRotation, defaultExposure);
+            colorGrading.brightness.value = defaultBrightness;
 
             return true;
         }
 
-        static void OnFixedUpdate(UnityModManager.ModEntry modEntry, float delta)
+        static void OnUpdate(UnityModManager.ModEntry modEntry, float delta)
 		{
             if (!FindSun())  return;
+            if (!findColorGrading()) return;
 
             DateTime now = DateTime.Now;
             TimeSpan timeSpanSinceMidnight = now.Subtract(new DateTime(now.Year, now.Month, now.Day));
-            float secondsSinceMidnight = (float)timeSpanSinceMidnight.TotalSeconds;
+            float secondsSinceMidnight = 0f; // 43200f; // (float)timeSpanSinceMidnight.TotalSeconds;
 
             float altitude = (nadirAltitude - culminationAltitude) / 2f
                 * Mathf.Cos(2f * Mathf.PI * secondsSinceMidnight / secondsInDay)
-                + (culminationAltitude - nadirAltitude) / 2f;
-            float azimuth = (sunriseAzimuth - sunsetAzimuth) / 2f
-                * Mathf.Sin(2f * Mathf.PI * secondsSinceMidnight / secondsInDay)
-                + (sunsetAzimuth + sunriseAzimuth) / 2f;
+                + (culminationAltitude + nadirAltitude) / 2f;
+            float azimuth = 360f * secondsSinceMidnight / secondsInDay - 180f;
             float intensity = dayIntensity;
             if (secondsSinceMidnight < dawnTime || secondsSinceMidnight > duskTime)
 			{
@@ -83,6 +131,51 @@ namespace DayNightMod
 			}
 
             UpdateSun(altitude, azimuth, intensity);
+
+            // this affects the VR loading environment, not the game environment!
+            //SteamVR_Skybox.SetOverride(skybox[0], skybox[1], skybox[2], skybox[3], skybox[4], skybox[5]);
+            // TODO: can the skybox texture be replaced at night?
+
+            float rotation = azimuth + 180f % 360f;
+            float ambientIntensity =  intensity * (dayIntensity - 0.16666f) / Mathf.Pow(dayIntensity, 2) + 0.16666f;
+            UpdateSkybox(rotation, ambientIntensity);
+
+            float brightness = intensity / dayIntensity - 1f;
+            // none of these help to dim the scenery:
+            //colorGrading.brightness.value = brightness;
+            //colorGrading.postExposure.value = brightness;
+            //colorGrading.masterCurve.value = new Spline(AnimationCurve.Linear(0, 0, 1, 0.16666f), 0, false, new Vector2(0, 1));
+
+            Debug.Log(string.Format("[Sun Settings]\n" +
+                "  >altitude={0}\n" +
+                "  >azimuth={1}\n" +
+                "  >intensity={2}\n" +
+                "  >rotation={3}\n" +
+                "  >ambient={4}\n" +
+                "  brightness={5}",
+                altitude,
+                azimuth,
+                intensity,
+                rotation,
+                ambientIntensity,
+                brightness));
+        }
+
+        static bool findColorGrading()
+		{
+            if (colorGrading == null)
+            {
+                PostProcessVolume ppv = UnityEngine.Object.FindObjectOfType<PostProcessVolume>();
+                if (ppv == null) return false;
+
+                ppv.profile.TryGetSettings<ColorGrading>(out colorGrading);
+                if (colorGrading == null) return false;
+
+                defaultBrightness = colorGrading.brightness.value;
+                Debug.Log("color grading brightness: " + colorGrading.brightness.value);
+            }
+
+            return true;
 		}
 
         static bool FindSun()
@@ -106,6 +199,13 @@ namespace DayNightMod
                     defaultAltitude,
                     defaultAzimuth,
                     defaultIntensity));
+
+                defaultRotation = RenderSettings.skybox.GetFloat("_Rotation");
+                defaultExposure = RenderSettings.skybox.GetFloat("_Exposure");
+
+                Debug.Log("skybox shader: " + RenderSettings.skybox.shader.name);
+                Debug.Log("skybox rotation: " + defaultRotation);
+                Debug.Log("skybox exposure: " + defaultExposure);
             }
 
             return true;
@@ -121,5 +221,11 @@ namespace DayNightMod
             sun.transform.rotation = Quaternion.Euler(angles);
             sun.intensity = intensity;
 		}
+
+        static void UpdateSkybox(float rotation, float exposure)
+		{
+            RenderSettings.skybox.SetFloat("_Rotation", rotation);
+            RenderSettings.skybox.SetFloat("_Exposure", exposure);
+        }
     }
 }
